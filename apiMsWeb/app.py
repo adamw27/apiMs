4f1ee36a71b419cbfaa9a66b19409f701693cd4f
adamw27 <96113236+adamw27@users.noreply.github.com>
2022-07-18 11:47:22 +0200

    Update app.py

--- apiMsWeb/app.py
+++ apiMsWeb/app.py
@@ -1,75 +1,84 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+import os
+from turtle import title
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 import requests
 
 url = 'https://jsonplaceholder.typicode.com'
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
-app.secret_key = "secretkey"
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/base')
-def base():
-    return render_template('base.html')
+db = SQLAlchemy(app)
 
-@app.route('/', methods=["POST", "GET"])
+class Post(db.Model):
+    postId = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+@app.route('/')
 def index():
-    if request.method == "POST":
-        if request.form.get("get") == "GET":
-            user_id = request.form["userId"]
-            post_id = request.form["postId"]
-            title = request.form["title"]
-            body = request.form["body"]
-            if post_id == 0:
-                response = requests.get(f"{url}/posts")
-            elif post_id >= 1 or post_id <= 100:
-                response = requests.get(f"{url}/posts/{post_id}")
-            else:
-                return render_template('index.html')
-            return response.text
-        elif request.form.get("post") == "POST":
-            user_id = request.form["userId"]
-            post_id = request.form["postId"]
-            title = request.form["title"]
-            body = request.form["body"]
-            user_valid = requests.get(f"{url}/users/{user_id}")
-            if user_valid.status_code == 200:
-                post_new = {
-                    "userId": user_id,
-                    "id": None,
-                    "title": title,
-                    "body": body
-                }
-                response = requests.post(f"{url}/posts", json=post_new)  # posting a new post
-            else:
-                return render_template('index.html')
-            return response.text
-        elif request.form.get("put") == "PUT":
-            user_id = request.form["userId"]
-            post_id = request.form["postId"]
-            title = request.form["title"]
-            body = request.form["body"]
-            user_valid = requests.get(f"{url}/users/{user_id}")
-            if user_valid.status_code == 200:
-                post_updated = {
-                    "userId": user_id,
-                    "id": None,
-                    "title": title,
-                    "body": body
-                }
-                response = requests.put(f"{url}/posts/{post_id}", json=post_updated)  # to update a post
-            else:
-                return render_template('index.html')
-            return response.text
-        elif request.form.get("delete") == "DELETE":
-            user_id = request.form["userId"]
-            post_id = request.form["postId"]
-            title = request.form["title"]
-            body = request.form["body"]
-            response = requests.delete(f"{url}/posts/{post_id}")  # print(response.status_code) to see if it got deleted
-            return str(response.status_code)
-        else:
-            return render_template('index.html')
-    else:
-        return render_template('index.html')
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
 
-if __name__=='__main__':
-    app.run(debug=True)
+@app.route('/<int:post_id>/')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        userId = int(request.form['userId'])
+        title = request.form['title']
+        body = request.form['body']
+        post = Post(userId=userId,
+                    title=title,
+                    body=body)
+        user_valid = requests.get(f"{url}/users/{userId}")
+        if user_valid.status_code == 200:
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+@app.route('/<int:post_id>/edit/', methods=('GET', 'POST'))
+def edit(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if request.method == 'POST':
+        postId = int(request.form['postId'])
+        userId = int(request.form['userId'])
+        title = request.form['title']
+        body = request.form['body']
+
+        post.postId = postId
+        post.userId = userId
+        post.title = title
+        post.body = body
+
+        user_valid = requests.get(f"{url}/users/{userId}")
+        if user_valid.status_code == 200:
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+@app.post('/<int:post_id>/delete/')
+def delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('index'))
